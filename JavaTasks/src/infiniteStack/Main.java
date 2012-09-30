39a7782375cfb35561c423e523da2a02559a36a6
Max <maxmayev@gmail.com>
2012-09-30 21:04:47 +0400

    Update JavaTasks/src/infiniteStack/Main.java
    Generics + iterator

--- JavaTasks/src/infiniteStack/Main.java
+++ JavaTasks/src/infiniteStack/Main.java
@@ -1,19 +1,37 @@
 package infiniteStack;
 
+import java.util.Iterator;
+
+
 public class Main {
     public static void main(String[] args){
-        StackNode stackNode1 = new StackNode("1");
-        StackNode stackNode2 = new StackNode("2");
-        StackNode stackNode3 = new StackNode("3");
-        StackNode stackNode4 = new StackNode("4");
 
-        InfiniteStack infiniteStack = new InfiniteStack();
-        infiniteStack.push(stackNode1);
-        infiniteStack.push(stackNode2);
-        infiniteStack.push(stackNode3);
-        infiniteStack.push(stackNode4);
+        InfiniteStack<String> infiniteStack = new InfiniteStack<String>();
+        infiniteStack.push("1");
+        infiniteStack.push("2");
+        infiniteStack.push("3");
+        infiniteStack.push("4");
+        infiniteStack.push("5");
+
+        System.out.println(infiniteStack.peek());
+
+        infiniteStack.pop();
+        System.out.println(infiniteStack.peek());
+
+        infiniteStack.push("hi");
+
+        System.out.println(infiniteStack.peek());
+
+        Iterator iterator = infiniteStack.iterator();
+        while (iterator.hasNext()){
+            StackNode<String> stackNode = (StackNode<String>)iterator.next();
+            System.out.println(stackNode.getValue());
+        }
 
         infiniteStack.pop();
         infiniteStack.pop();
+        System.out.println(infiniteStack.peek());
+
+
     }
 }
